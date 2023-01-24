c22417d86b53bde955a0e27ee9bc22cd95528963
Andrey Denisov <86472463+Andr-Den@users.noreply.github.com>
2023-01-24 21:09:08 +0500

    add styles for comments block

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,15 @@
 const comments = [
   {
-    image: '',
-    name: 'Николай Михайлович Ильяшенко',
-    text: 'Получаем постоянное внимание со стороны менеджеров - при том, что нам редко требуются какие-либо работы по сайту на BLIZKO, ассортимент у нас стабильный, цены договорные. Размещаемся мы на BLIZKO только в Краснодаре, а заявки к нам поступают и из Хабаровска, и из Калининграда - мы продаем высокотехнологичные полимеры для ремонта и защиты различных материалов, таких сайтов всего несколько в стране, поэтому нас отлично находят.',
+    name: 'Александр Ф.',
+    text: 'Доступный, эффективный и не очень дорогой препарат. Необязательно пить целый курс - можно принимать разово при необходимости.',
   },
   {
-    image: '',
-    name: 'Сайдмагомедова Яна Сергеевна',
-    text: 'Нам нравится работа всех сотрудников, с которыми мы успели поработать за несколько лет. Нам всегда оперативно отвечают и помогают. Что касается сайта, то с ним довольно удобно работать, все интуитивно понятно. По выдаче: в поисковие blizko.ru по многим нашим ключевым запросам мы в первых строках, это большой плюс.',
+    name: 'Петр И.',
+    text: 'Помогает практически сразу, длительный эффект.',
+  },
+  {
+    name: 'Василий Н.',
+    text: 'Эффект после первого приема',
   },
 ]
 
@@ -15,8 +17,7 @@ let commentImage = document.getElementById("comment-image");
 let commentName = document.getElementById("comment-name");
 let commentText = document.getElementById("comment-text");
 
-
-commentImage.innerHTML = `${comments[0].image}`
+commentImage.style.backgroundImage = `url(./images/1.webp)`
 commentName.innerHTML = `${comments[0].name}`
 commentText.innerHTML = `${comments[0].text}`
 
@@ -26,23 +27,23 @@ let back = document.getElementById("back");
 let value = 0
 
 next.onclick = function() {
-  if (value === 1) {
+  if (value === 2) {
     value = 0
   } else {
     value++
   }
-  commentImage.innerHTML = `${comments[value].image}`
+  commentImage.style.backgroundImage = `url(./images/${value+1}.webp)`
   commentName.innerHTML = `${comments[value].name}`
   commentText.innerHTML = `${comments[value].text}`
 }
 
 back.onclick = function() {
   if (value === 0) {
-    value = 1
+    value = 2
   } else {
     value--
   }
-  commentImage.innerHTML = `${comments[value].image}`
+  commentImage.style.backgroundImage = `url(./images/${value+1}.webp)`
   commentName.innerHTML = `${comments[value].name}`
   commentText.innerHTML = `${comments[value].text}`
 }
